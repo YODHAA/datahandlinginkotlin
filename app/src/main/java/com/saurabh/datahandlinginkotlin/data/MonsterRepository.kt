236b08124cc51e18336ebea6b2152e27c0c5991a
Yodhaa <saurabhsingh4589@gmail.com>
2020-09-20 01:54:00 +0530

    Implemented LiveDataModel

--- app/src/main/java/com/saurabh/datahandlinginkotlin/data/MonsterRepository.kt
+++ app/src/main/java/com/saurabh/datahandlinginkotlin/data/MonsterRepository.kt
@@ -1,24 +1,31 @@
 package com.saurabh.datahandlinginkotlin.data
 
+import android.app.Application
 import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import com.saurabh.datahandlinginkotlin.utilities.FileHelper
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
-class MonsterRepository {
+class MonsterRepository(val app:Application) {
+
+    val monsterData = MutableLiveData<List<Monster>>()
 
     private val listType = Types.newParameterizedType(
         List::class.java , Monster::class.java
     )
+    init {
+        getMonsterData()
+    }
 
-     fun getMonsterData(context: Context) : List<Monster> {
-         val text = FileHelper.getTextFromAssets(context, "monster_data.json")
+     fun getMonsterData() {
+         val text = FileHelper.getTextFromAssets(app, "monster_data.json")
         val moshi = Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
         val adapter : JsonAdapter<List<Monster>> = moshi.adapter(listType)
-        return adapter.fromJson(text)?: emptyList()
+        monsterData.value =  adapter.fromJson(text)?: emptyList()
     }
 }
