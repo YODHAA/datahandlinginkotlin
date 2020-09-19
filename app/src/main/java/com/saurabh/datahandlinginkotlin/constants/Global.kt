054e0fee93092bed2ff7f4778adef9eb6d7cc5c7
Yodhaa <saurabhsingh4589@gmail.com>
2020-09-19 21:13:25 +0530

    Added Main Fragment Layout

---
+++ app/src/main/java/com/saurabh/datahandlinginkotlin/constants/Global.kt
@@ -0,0 +1,3 @@
+package com.saurabh.datahandlinginkotlin.constants
+
+const val LOG_TAG = "monsterLogging"
--- app/src/main/java/com/saurabh/datahandlinginkotlin/ui/main/MainFragment.kt
+++ app/src/main/java/com/saurabh/datahandlinginkotlin/ui/main/MainFragment.kt
@@ -1,10 +1,37 @@
 package com.saurabh.datahandlinginkotlin.ui.main
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.*
+import com.saurabh.datahandlinginkotlin.R
+import com.saurabh.datahandlinginkotlin.constants.LOG_TAG
+import com.saurabh.datahandlinginkotlin.data.Monster
 
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val monster = Monster("Bob", "myfile", "a caption",
+            "a description", .19, 3)
+        Log.i(LOG_TAG, monster.toString())
+        return inflater.inflate(R.layout.main_fragment,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 }
